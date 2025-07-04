cea5c667533b61c4bcc3871fea9da5bb3356e83a
maksosck <140080228+makssxwq@users.noreply.github.com>
2025-07-05 00:00:17 +0300

    v0.0.2

--- cmd/main.go
+++ cmd/main.go
@@ -4,40 +4,52 @@ import (
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
+	"net/url"
 	"shortURL/internal/db"
 )
 
+func isURL(link string) bool {
+	u, err := url.Parse(link)
+	if err != nil {
+		return false
+	}
+	return (u.Opaque != "" || u.Host != "") && u.Scheme != ""
+}
 
 func main() {
 	router := gin.Default()
 
-	// Подгружаем HTML файлы
+	// Load HTML
 	router.Static("/static", "../public")
 	router.LoadHTMLFiles("../public/index.html")
 
-	// Обработчик для / запроса
+	// / Page handler
 	router.GET("/", func (c *gin.Context) {
 		c.HTML(http.StatusOK,"index.html", nil)
 	})
 
-	// Запрос сокращения ссылки
+	// Link shortener handler
 	router.POST("/short", func(c *gin.Context) {
 		url := c.PostForm("link")
-		log.Println("получено: ", url)
+		if isURL(url) {
+			log.Println("получено: ", url)
 
-		if url == "" {
-			 c.JSON(http.StatusBadRequest, gin.H{"error": "Пустая ссылка"})
-			 return
+			if url == "" {
+				c.JSON(http.StatusBadRequest, gin.H{"error": "Пустая ссылка"})
+				return
+			}
+
+			short, _ := db.AddLink(url)
+			c.JSON(http.StatusOK, gin.H{
+				"short": short,
+				"link": url,
+				})
+		} else {
+			c.JSON(http.StatusBadRequest, gin.H{"error" : "Bad link"})
 		}
-
-		short, _ := db.AddLink(url)
-		c.JSON(http.StatusOK, gin.H{
-			"short": short,
-			"link": url,
-		})
 	})
 
-	// Переадресация на оригинальную ссылку
+	// Pereadresation
 	router.GET("/:id", func(c *gin.Context) {
 			id := c.Param("id")
 			link, _ := db.GetLink(id)
@@ -45,5 +57,5 @@ func main() {
 			c.Redirect(http.StatusFound, link)
 	})
 
-	router.Run(":3456")
+	router.Run(":4040")
 }
